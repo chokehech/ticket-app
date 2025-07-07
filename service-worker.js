942c16c7cda7fb9bdc7663fb32a90c8d1c5f4aa5
chokehech <103856977+chokehech@users.noreply.github.com>
2025-07-07 10:13:08 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bus-trip-v5"; // Increment this to force update
+const CACHE_NAME = "bus-trip-v6"; // Increment this to force update
 const urlsToCache = [
   "./",
   "./index.html",
